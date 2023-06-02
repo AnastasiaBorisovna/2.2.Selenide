7fdadf845fc5f1106e70a058f055126ed555311f
Emma Paris <eparis@atlassian.com>
2023-06-02 07:08:32 +0300

    Gitignore

---
+++ src/test/java/SelenideTest.java
@@ -0,0 +1,37 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTest {
+    String generateDate(int daysToAdd, String pattern) {
+        return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern(pattern));
+
+    }
+
+    @Test
+    public void registrationCardTest() {
+        open("http://localhost:9999/");
+        $("[placeholder='Город']").setValue("Самара");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(generateDate(3, "dd.MM.yyyy"));
+        $("[name='name']").setValue("Семенова Анна Аркадьевна");
+        $("[name='phone']").setValue("+79256584856");
+        $("[data-test-id='agreement']").click();
+        $$("[type='button']").find(Condition.exactText("Забронировать")).click();
+        String planningDate = generateDate(3, "dd.MM.yyyy");
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+    }
+
+
+}
+
